a6a36d96982221c6985473d90be46ece056f39b1
Cameron Barker <barker.cameron@gmail.com>
2015-09-05 11:30:02 -0400

    adding skils and removing modal

--- app/scripts/controllers/barker.js
+++ app/scripts/controllers/barker.js
@@ -8,82 +8,11 @@
  * Controller of the parkerBarkerApp
  */
 angular.module('parkerBarkerApp')
-  .controller('BarkerCtrl', function ($scope, barkerProjects) {
+  .controller('BarkerCtrl', function ($scope, barkerProjects, barkerSkills) {
     barkerProjects.success(function(data) {
       $scope.projects = data;
     });
-
-    $scope.languages = [
-      "Ruby",
-      "Swift",
-      "Javascript",
-      "Jquery",
-      "Sass",
-      "Haml"
-    ];
-
-    $scope.frameworks = [
-      "Ruby on Rails",
-      "iOS",
-      "Shopify",
-      "Bootstrap",
-      "Materialize"
-    ];
-
-    $scope.databases = [
-      "Postgres",
-      "Redis",
-      "ElasticSearch",
-      "Solr"
-    ];
-
-    $scope.learning = [
-      "Coffeescript",
-      "Node",
-      "Express.js",
-      "Angular",
-      "Monogoid",
-      "Python",
-      "Go"
-    ];
-
-    $scope.applications = [
-      "Unity3D",
-      "Photoshop",
-      "Atom"
-    ];
-
-    $scope.education = [
-      "University of Utah BS 2010",
-      "GA: Data Science 2015",
-      "GA: Product Managment 2016"
-    ];
-
-    $scope.meetups = [
-      "NY.rb"
-    ];
-
-    $scope.interests = [
-      "Family", "Films", "Wood Working", "Robots", "Reading"
-    ];
-
-    $scope.mission = "Be Somebody";
-    $scope.personal_goals = "Positive, Patient, Persistant";
-
-    $scope.modal = {
-      title: "cat",
-      description: "",
-      url: "",
-      job_title: "",
-      myInterval: 3000,
-      slides: []
-    };
-
-    // $scope.updateModal = function(obj){
-    //   $scope.modal.title = obj.title;
-    //   $scope.modal.description = obj.description;
-    //   $scope.modal.url = obj.url;
-    //   $scope.modal.job_title = obj.job_title;
-    //   // $scope.modal.slides = obj.images;
-    // };
+    barkerSkills.success(function(data) {
+      $scope.skills = data;
+    });
   });
