52f0149923306d0f29492862cdd3e8e1c2d22f47
Cameron Barker <barker.cameron@gmail.com>
2015-09-05 11:43:51 -0400

    min images

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -11,18 +11,18 @@ angular.module('parkerBarkerApp')
   .controller('HomeCtrl', function ($scope, $location) {
     $scope.cameron = {
       name: "barker",
-      image: "images/portrait/him.jpg",
-      logoImage: "images/logo/barker.png",
-      logoOriginal: "images/logo/barker.png",
-      logoHover: "images/logo/barker-white.png",
+      image: "images/portrait/him-min.jpg",
+      logoImage: "images/logo/barker-min.png",
+      logoOriginal: "images/logo/barker-min.png",
+      logoHover: "images/logo/barker-white-min.png",
       url: "barker"
     };
     $scope.hayley = {
       name: "parker",
-      image: "images/portrait/her.jpg",
-      logoImage: "images/logo/parker.png",
-      logoOriginal: "images/logo/parker.png",
-      logoHover: "images/logo/parker-white.png",
+      image: "images/portrait/her-min.jpg",
+      logoImage: "images/logo/parker-min.png",
+      logoOriginal: "images/logo/parker-min.png",
+      logoHover: "images/logo/parker-white-min.png",
       url: "parker",
     };
     $scope.changeView = function(view){
